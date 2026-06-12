e3977e64f4037b9aaaffea8b888cdf95bc5b0559
Priyanka-frontend <kaushikpriyanka005@gmail.com>
2026-06-12 12:48:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,23 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
+const prices = document.querySelectorAll(".prices");
+
+  let total = 0;
+
+  prices.forEach((price) => {
+    total += Number(price.textContent);
+  });
+
+  const tr = document.createElement("tr");
+  const td = document.createElement("td");
+
+  td.textContent = total;
+  td.colSpan = 2; // optional, makes the total cell span columns
+
+  tr.appendChild(td);
+
+  document.querySelector("table").appendChild(tr);
   
 };
 
